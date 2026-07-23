d0b113e68f245587059c6f7478150a741beeb812
STipen-student <hudathoni@gmail.com>
2026-07-23 12:32:16 +0700

    fix: load upright building 3D corrections

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./campaign.css";
 import "./city-3d.css";
+import "./city-3d-fix.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
